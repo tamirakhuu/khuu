a1a89e0dd657d89c2fb1653cc6429bbba63150ce
tamirakhuu <tamirakhuu@gmail.com>
2026-04-07 18:02:22 +0800

    d

--- khuu.js
+++ khuu.js
@@ -37,7 +37,7 @@ const PRODUCTS = [
     size: ["M", "L", "XL"],
     stock: true,
     image:
-      "assets/cargo.png",
+      "src/assets/cargo.png",
     description: "Streetwear хэв маягтай олон халаастай cargo өмд.",
   },
   {
