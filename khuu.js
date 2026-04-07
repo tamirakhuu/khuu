770433d27dfe1e0172daaada5cc6157f1faaaf7c
tamirakhuu <tamirakhuu@gmail.com>
2026-04-07 18:11:53 +0800

    c

--- khuu.js
+++ khuu.js
@@ -5,7 +5,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Badge } from "@/components/ui/badge";
-import cargoImg from "../khuu/src/assets/cargo.png";
+import cargoImg from "../assets/cargo.png";
 
 const PRODUCTS = [
   {
