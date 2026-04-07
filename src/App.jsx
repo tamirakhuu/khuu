97123bf80872792229e8690315f63e24c2541f0c
tamirakhuu <tamirakhuu@gmail.com>
2026-04-07 19:27:50 +0800

    fixxx

--- src/App.jsx
+++ src/App.jsx
@@ -328,20 +328,6 @@ const addToCart = (product, chosenSize = "") => {
   setCartOpen(true);
 };
 
-    setCart((prev) => {
-      const found = prev.find((item) => item.id === product.id);
-      if (found) {
-        return prev.map((item) =>
-          item.id === product.id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        );
-      }
-      return [...prev, { ...product, quantity: 1 }];
-    });
-    setCartOpen(true);
-  };
-
   const changeQuantity = (id, delta) => {
     setCart((prev) =>
       prev
